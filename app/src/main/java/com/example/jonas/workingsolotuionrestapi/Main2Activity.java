22790505a17c43bdab33363f87edd3f4d319483c
jbitsme <jonas_husted@live.dk>
2018-08-29 10:24:19 +0200

    working
    the reciving data emulator
    skype on phone with skype installed

---
+++ app/src/main/java/com/example/jonas/workingsolotuionrestapi/Main2Activity.java
@@ -0,0 +1,57 @@
+package com.example.jonas.workingsolotuionrestapi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        Button makeSkypeCall = (Button) findViewById(R.id.makeSkypeCall);
+        final EditText SIPAddress = (EditText) findViewById(R.id.SIPAddress);
+        final CheckBox videoCall = (CheckBox) findViewById(R.id.videoCheck);
+        makeSkypeCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String uriString = "ms-sfb://call?id=" + SIPAddress.getText().toString();
+                if (videoCall.isChecked()) {
+                    uriString += "&video=true";
+                }
+                Uri uri = Uri.parse(uriString);
+                Intent callIntent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(callIntent);
+            }
+        });
+
+        Button skypeMessageButton = (Button) findViewById(R.id.makeSkypeMessage);
+        skypeMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uriString = "ms-sfb://chat?id=" + SIPAddress.getText().toString();
+                Uri uri = Uri.parse(uriString);
+                Intent callIntent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(callIntent);
+            }
+        });
+
+        Button backButton = (Button) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/jonas/workingsolotuionrestapi/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.jonas.workingsolotuionrestapi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+    public static TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.button);
+        data = (TextView) findViewById(R.id.data);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });
+
+        Button skype = (Button) findViewById(R.id.skypeWindow);
+
+        skype.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+    }
+}
